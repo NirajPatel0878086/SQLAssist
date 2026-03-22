cc9d666ad0ff78db7706e2290af7a7a66bf9f59d
“niraj <nirajpatel7478@gmail.com>
2026-03-22 12:55:27 -0400

    Fixed database setup created tables.

--- src/main/java/com/example/sqlassist/database/DBConst.java
+++ src/main/java/com/example/sqlassist/database/DBConst.java
@@ -2,45 +2,43 @@ package com.example.sqlassist.database;
 
 public class DBConst {
 
-    public static final String TABLE_USERS = "app_users";
+    public static final String TABLE_USERS = "users";
     public static final String USER_COL_ID = "id";
-    public static final String USER_COL_FIRST = "first_name";
-    public static final String USER_COL_LAST = "last_name";
+    public static final String USER_COL_NAME = "name";
     public static final String USER_COL_EMAIL = "email";
-    public static final String USER_COL_PASS = "password";
 
-    public static final String TABLE_ITEMS = "managed_items";
+    public static final String TABLE_CATEGORIES = "categories";
+    public static final String CATEGORY_COL_ID = "id";
+    public static final String CATEGORY_COL_NAME = "name";
+
+    public static final String TABLE_ITEMS = "items";
     public static final String ITEM_COL_ID = "id";
-    public static final String ITEM_COL_NAME = "item_name";
-    public static final String ITEM_COL_TYPE = "item_type";
-    public static final String ITEM_COL_VALUE = "item_value";
-
-    public static final String TABLE_LOGS = "activity_logs";
-    public static final String LOG_COL_ID = "id";
-    public static final String LOG_COL_ACTION = "action_name";
-    public static final String LOG_COL_DATE = "action_date";
+    public static final String ITEM_COL_NAME = "name";
+    public static final String ITEM_COL_QUANTITY = "quantity";
+    public static final String ITEM_COL_PRICE = "price";
+    public static final String ITEM_COL_CATEGORY_ID = "category_id";
 
     public static final String CREATE_TABLE_USERS =
             "CREATE TABLE IF NOT EXISTS " + TABLE_USERS + " (" +
-                    USER_COL_ID + " int NOT NULL AUTO_INCREMENT, " +
-                    USER_COL_FIRST + " VARCHAR(50), " +
-                    USER_COL_LAST + " VARCHAR(50), " +
-                    USER_COL_EMAIL + " VARCHAR(100), " +
-                    USER_COL_PASS + " VARCHAR(100), " +
-                    "PRIMARY KEY(" + USER_COL_ID + "))";
+                    USER_COL_ID + " INT NOT NULL AUTO_INCREMENT, " +
+                    USER_COL_NAME + " VARCHAR(100) NOT NULL, " +
+                    USER_COL_EMAIL + " VARCHAR(100) NOT NULL, " +
+                    "PRIMARY KEY (" + USER_COL_ID + "))";
+
+    public static final String CREATE_TABLE_CATEGORIES =
+            "CREATE TABLE IF NOT EXISTS " + TABLE_CATEGORIES + " (" +
+                    CATEGORY_COL_ID + " INT NOT NULL AUTO_INCREMENT, " +
+                    CATEGORY_COL_NAME + " VARCHAR(100) NOT NULL, " +
+                    "PRIMARY KEY (" + CATEGORY_COL_ID + "))";
 
     public static final String CREATE_TABLE_ITEMS =
             "CREATE TABLE IF NOT EXISTS " + TABLE_ITEMS + " (" +
-                    ITEM_COL_ID + " int NOT NULL AUTO_INCREMENT, " +
-                    ITEM_COL_NAME + " VARCHAR(100), " +
-                    ITEM_COL_TYPE + " VARCHAR(50), " +
-                    ITEM_COL_VALUE + " VARCHAR(100), " +
-                    "PRIMARY KEY(" + ITEM_COL_ID + "))";
-
-    public static final String CREATE_TABLE_LOGS =
-            "CREATE TABLE IF NOT EXISTS " + TABLE_LOGS + " (" +
-                    LOG_COL_ID + " int NOT NULL AUTO_INCREMENT, " +
-                    LOG_COL_ACTION + " VARCHAR(100), " +
-                    LOG_COL_DATE + " VARCHAR(50), " +
-                    "PRIMARY KEY(" + LOG_COL_ID + "))";
+                    ITEM_COL_ID + " INT NOT NULL AUTO_INCREMENT, " +
+                    ITEM_COL_NAME + " VARCHAR(100) NOT NULL, " +
+                    ITEM_COL_QUANTITY + " INT NOT NULL, " +
+                    ITEM_COL_PRICE + " DOUBLE NOT NULL, " +
+                    ITEM_COL_CATEGORY_ID + " INT NOT NULL, " +
+                    "PRIMARY KEY (" + ITEM_COL_ID + "), " +
+                    "FOREIGN KEY (" + ITEM_COL_CATEGORY_ID + ") REFERENCES " +
+                    TABLE_CATEGORIES + "(" + CATEGORY_COL_ID + "))";
 }
